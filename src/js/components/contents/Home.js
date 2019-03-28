63eaeb0afe0b7fbb7387e9259d43e2c77cc52d9e
Morris Lin <snoopy90338@gmail.com>
2019-03-28 11:58:32 +0800

    add animation

--- src/js/components/contents/Home.js
+++ src/js/components/contents/Home.js
@@ -4,7 +4,7 @@ export default class Home extends Component {
 	render() {
 		return(
 			<div className='fl-row w-100p'>
-				<div className='font-pri w-50p mr-100 f3'>
+				<div className='font-pri w-50p mr-100 f3 animated aminate-fade-in fade-in-up'>
 					<h1>Hello,</h1>
 					<h1>I'm Lin Cheng Wei</h1>
 				</div>
