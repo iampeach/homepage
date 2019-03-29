3abd1b36c46cdc0e1135a669a5765369ad7d0c41
Morris Lin <snoopy90338@gmail.com>
2019-03-29 11:20:26 +0800

    simple contact

--- src/js/components/contents/Contact.js
+++ src/js/components/contents/Contact.js
@@ -3,7 +3,11 @@ import React, { Component } from 'react'
 export default class Contact extends Component {
 	render() {
 		return(
-			<h1>Contact</h1>
+			<div className='fl-col align-center'>
+				<h1 className='font-pri f2 animated animate-fade-in fade-in-up'>Contact me</h1>
+				<h2 className='font-sec f3 animated animate-fade-in fade-in-up delay-05'>snoopy90338@gmail.com</h2>
+				{/*<h2 className='sec-font'>b05901061@ntu.edu.tw</h2>*/}
+			</div>
 		)
 	}
 }
