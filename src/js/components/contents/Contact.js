a1536552f1f054c8201b2527ca3cd664731cf88d
Morris Lin <snoopy90338@gmail.com>
2019-03-29 20:33:02 +0800

    simple contact

--- src/js/components/contents/Contact.js
+++ src/js/components/contents/Contact.js
@@ -2,10 +2,11 @@ import React, { Component } from 'react'
 
 export default class Contact extends Component {
 	render() {
+		var style = this.props.className.concat(' fl-col align-center')
 		return(
-			<div className='fl-col align-center'>
+			<div className={style}>
 				<h1 className='font-pri f2 animated animate-fade-in fade-in-up'>Contact me</h1>
-				<h2 className='font-sec f3 animated animate-fade-in fade-in-up delay-05'>snoopy90338@gmail.com</h2>
+				<h2 className='font-sec f3 animated animate-fade-in fade-in-up delay-03'>snoopy90338@gmail.com</h2>
 				{/*<h2 className='sec-font'>b05901061@ntu.edu.tw</h2>*/}
 			</div>
 		)
