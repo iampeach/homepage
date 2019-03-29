f8a5e4db0f3981b08824fa2c76300eaa447f5567
Morris Lin <snoopy90338@gmail.com>
2019-03-29 11:20:47 +0800

    add subtitle

--- src/js/components/contents/Home.js
+++ src/js/components/contents/Home.js
@@ -4,9 +4,10 @@ export default class Home extends Component {
 	render() {
 		return(
 			<div className='fl-row w-100p'>
-				<div className='font-pri w-50p mr-100 f3 animated aminate-fade-in fade-in-up'>
-					<h1>Hello,</h1>
-					<h1>I'm Lin Cheng Wei</h1>
+				<div className='font-pri w-50p mr-100 animated aminate-fade-in fade-in-up'>
+					<h1 className='f2'>Hello,</h1>
+					<h1 className='f2'>I'm Lin Cheng Wei</h1>
+					<h2 className='f4 grey light'>A student current studying in NTUEE</h2>
 				</div>
 				<div className='w-50p'>Image here.</div>
 			</div>
