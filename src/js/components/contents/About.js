c6eb976f449b2259f4d64669ecd050137c2da819
Morris Lin <snoopy90338@gmail.com>
2019-03-29 20:30:41 +0800

    add simple about

--- src/js/components/contents/About.js
+++ src/js/components/contents/About.js
@@ -1,9 +1,31 @@
 import React, { Component } from 'react'
+import AboutBlock from './about-components/AboutBlock'
+import { setDelay } from '../../utils/animation'
 
 export default class About extends Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			text: [
+				{
+					title: 'I am a junior student stydying in Electrical Engineering, National Taiwan University.',
+					content: 'Interested in EDA and Machine Learning and Web Design.'
+				},
+				{
+					title: 'Also interested in multimedia and photographing.',
+					content: 'Delight in watching movie and also create a short films.'
+				}
+			]
+		}
+	}
 	render() {
+		var abouts = this.state.text.map((text,idx) => 
+			<AboutBlock key={idx} delay={setDelay(5*idx)} text={text}/>)
+		var style = this.props.className.concat(' fl-col align-stretch')
 		return(
-			<h1>About</h1>
+			<div className={style}>
+				{abouts}
+			</div>
 		)
 	}
 }
